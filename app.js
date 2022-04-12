04cb44777ee320b141ef068b5bbf4c21b947e551
reuben-k-o <rubenkhaemba@gmail.com>
2022-04-12 20:15:34 +0300

    union types

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
+"use strict";
+const num1El = document.getElementById('num1');
+const num2El = document.getElementById('num2');
+const buttonEl = document.querySelector('button');
 function add(num1, num2) {
-    return num1 + num2;
+    if (typeof num1 === 'number' && typeof num2 === 'number') {
+        return num1 + num2;
+    }
+    else if (typeof num1 === 'string' && typeof num2 === 'string') {
+        return num1 + ' ' + num2;
+    }
+    else {
+        return +num1 + +num2;
+    }
 }
-console.log(add(1, 6));
-console.log(add('1', '6'));
+buttonEl.addEventListener('click', () => {
+    const val1 = num1El.value;
+    const val2 = num2El.value;
+    const resNumber = add(+val1, +val2);
+    const resString = add(val1, val2);
+    console.log(resNumber, resString);
+});
+// console.log(add(1, 6));
+// console.log(add('1', '6'));
