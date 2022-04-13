1bdb792ffc66b2c3096d1fdea2b02d3faa0db3b3
reuben-k-o <rubenkhaemba@gmail.com>
2022-04-13 05:44:09 +0300

    type aliases and interfaces

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ function add(num1, num2) {
         return +num1 + +num2;
     }
 }
+const numRes = [];
+const strRes = [];
 function printResult(resultObj) {
     console.log(resultObj.val);
 }
@@ -20,9 +22,12 @@ buttonEl.addEventListener('click', () => {
     const val1 = num1El.value;
     const val2 = num2El.value;
     const resNumber = add(+val1, +val2);
+    numRes.push(resNumber);
     const resString = add(val1, val2);
+    strRes.push(resString);
     console.log(resNumber, resString);
     printResult({ val: resNumber, timestamp: new Date() });
+    console.log(numRes, strRes);
 });
 // console.log(add(1, 6));
 // console.log(add('1', '6'));
--- app.ts
+++ app.ts
@@ -2,8 +2,15 @@ const num1El = document.getElementById('num1') as HTMLInputElement;
 const num2El = document.getElementById('num2') as HTMLInputElement
 const buttonEl = document.querySelector('button')!
 
+type NumOrString = number | string
+type Result = { val: number; timestamp: Date }
 
-function add(num1: number | string, num2: number | string) {
+interface ResultObj {
+  val: number;
+  timestamp: Date
+}
+
+function add(num1: NumOrString, num2: NumOrString) {
   if (typeof num1 === 'number' && typeof num2 === 'number') {
     return num1 + num2
   } else if (typeof num1 === 'string' && typeof num2 === 'string') {
@@ -13,8 +20,11 @@ function add(num1: number | string, num2: number | string) {
     return +num1 + +num2;
   }
 }
+const numRes: number[] = []
+const strRes: string[] = []
 
-function printResult(resultObj: { val: number; timestamp: Date }) {
+
+function printResult(resultObj: Result) {
   console.log(resultObj.val)
 }
 
@@ -22,9 +32,12 @@ buttonEl.addEventListener('click', () => {
   const val1 = num1El.value;
   const val2 = num2El.value;
   const resNumber = add(+val1, +val2)
+  numRes.push(resNumber as number)
   const resString = add(val1, val2)
+  strRes.push(resString as string)
   console.log(resNumber, resString)
   printResult({ val: resNumber as number, timestamp: new Date() })
+  console.log(numRes, strRes)
 })
 
 // console.log(add(1, 6));
